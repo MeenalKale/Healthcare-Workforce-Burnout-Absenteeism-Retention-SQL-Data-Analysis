73763fbe4eee9b97790fab7b0fa9d03751f2ee92
MeenalKale <meenalk2005@gmail.com>
2026-01-28 15:06:35 +0400

    Update schema.sql

--- data/schema.sql
+++ data/schema.sql
@@ -1,1 +1,35 @@
-Table: Healthcare Workforce Mental Health Dataset 
+TABLE: "Healthcare Workforce Mental Health Dataset" 
+
+COLUMN:
+Employee ID (TEXT)
+Employee Type (TEXT)
+Department (TEXT)
+Workplace Factor (TEXT)
+Stress Level (INTEGER)
+Burnout Frequency (TEXT)
+Job Satisfaction (INTEGER)
+Access to EAPs (TEXT)
+Mental Health Absences (INTEGER)
+Turnover Intentions (TEXT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
